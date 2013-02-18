93d908aa165ae7ba36bc14a8f6e828e6dee79a4c
Adrian <adrian@Admins-MacBook-Pro.local>
2013-02-18 19:36:05 +0000

    Made a galaxy grid using a canvas, intending to add touch events next

--- src/digitalrunners/game/gc/Start.java
+++ src/digitalrunners/game/gc/Start.java
@@ -4,14 +4,21 @@ import com.example.gc.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
 
 public class Start extends Activity {
 
+	GalaxyView gv;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        gv = new GalaxyView(this);
+        setContentView(gv);
+        
     }
 
 
