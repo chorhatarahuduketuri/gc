a908f64c704ead02836d9b7e91eb1003558d7aa1
Adrian <adrian@Admins-MacBook-Pro.local>
2013-02-19 20:11:51 +0000

    Added test sprite to test drawing capabilities and a basic ship class

--- src/dadeindustries/game/gc/GalaxyView.java
+++ src/dadeindustries/game/gc/GalaxyView.java
@@ -1,10 +1,16 @@
 package dadeindustries.game.gc;
 
+import java.util.ArrayList;
+
+import com.example.gc.R;
+
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,79 +20,97 @@ import android.widget.Toast;
 public class GalaxyView extends View implements OnTouchListener {
 
 	Context ctxt;
-	
-	Paint paint = new Paint();
-	int displayWidth  = 0;
-	int displayHeight = 0;
-	int squareSize = 0;
-	Bitmap b; // temp variable
 
-	
+	private final static int NUM_SQUARES_IN_ROW = 5;
+	private Paint paint = new Paint();
+	private int displayWidth = 0;
+	private int displayHeight = 0;
+	private static int SQUARE_SIZE;
+	Bitmap ship = null; // temp variable
+
+	ArrayList<Ship> ships = new ArrayList<Ship>();
+
 	public GalaxyView(Context context) {
 		super(context);
 		ctxt = context;
 		DisplayMetrics metrics = getResources().getDisplayMetrics();
 		displayWidth = metrics.widthPixels;
 		displayHeight = metrics.heightPixels;
-		squareSize = displayWidth / 6;
+		SQUARE_SIZE = displayWidth / NUM_SQUARES_IN_ROW;
 		this.setBackgroundColor(Color.BLACK);
-	    paint.setColor(Color.WHITE);
-	    paint.setStrokeWidth(3);
-	    this.setOnTouchListener(this);
-	  //  new Bitmap(R.drawable.bp);
+		paint.setColor(Color.WHITE);
+		paint.setStrokeWidth(3);
+		ship = BitmapFactory.decodeResource(getResources(), R.drawable.ship);
+		ships.add(new Ship(SQUARE_SIZE * 2, SQUARE_SIZE * 2));
+
+		this.setOnTouchListener(this);
 
 	}
+
+	Rect r = new Rect();
 
 	@Override
 	public void onDraw(Canvas canvas) {
 
 		// vertical lines
-		for(int i=1; i<=displayWidth; i++){
-			canvas.drawLine(i*squareSize, 0, i*squareSize, displayHeight, paint);
+		for (int i = 1; i <= displayWidth; i++) {
+			canvas.drawLine(i * SQUARE_SIZE, 0, i * SQUARE_SIZE, displayHeight,
+					paint);
 		}
 
 		// horizontal lines
-		for(int k=1; k<displayHeight; k++){
-			canvas.drawLine(0, k*squareSize, displayWidth, k*squareSize, paint);
+		for (int k = 1; k < displayHeight; k++) {
+			canvas.drawLine(0, k * SQUARE_SIZE, displayWidth, k * SQUARE_SIZE,
+					paint);
 		}
-		
+
+		// draw current icons
+		// canvas.drawBitmap(bitmap, NULL, dst, paint);
+
+		for (int i = 0; i < ships.size(); i++) {
+			int x = ships.get(0).x;
+			int y = ships.get(0).y;
+			r.left = x;
+			r.top = y;
+			r.right = x + SQUARE_SIZE;
+			r.bottom = y + SQUARE_SIZE;
+			canvas.drawBitmap(ship, null, r, paint);
+		}
+
 		// highlight current square
-		if(currentX>=0 && currentY>=0)
-		{
+		if (currentX >= 0 && currentY >= 0) {
 			paint.setColor(Color.RED);
 			paint.setStyle(Paint.Style.STROKE);
 
-			int a = currentX*squareSize;
-			int b = currentY*squareSize;
-		    paint.setStrokeWidth(6);
-			canvas.drawRect(a, b, a+squareSize, b+squareSize, paint);
-		    paint.setStrokeWidth(3);
+			int a = currentX * SQUARE_SIZE;
+			int b = currentY * SQUARE_SIZE;
+			paint.setStrokeWidth(6);
+			canvas.drawRect(a, b, a + SQUARE_SIZE, b + SQUARE_SIZE, paint);
+			paint.setStrokeWidth(3);
 			paint.setStyle(Paint.Style.FILL);
 			paint.setColor(Color.WHITE);
-			
 
 		}
-		
-		// 	draw current icons		
-		//	canvas.drawBitmap(bitmap, NULL, dst, paint);		
-	}
+
 	
+	
+	}
+
 	private int currentX = -1;
 	private int currentY = -1;
-	
+
 	@Override
 	public boolean onTouch(View view, MotionEvent motion) {
 
-		// Use the type of motion event to potentially figure out 
+		// Use the type of motion event to potentially figure out
 		// whether user wishes to scroll map
-		
-		if(motion.getActionMasked()==MotionEvent.ACTION_MOVE)
-		{
-			Toast.makeText(ctxt, "Movement!", Toast.LENGTH_SHORT).show();
+
+		if (motion.getActionMasked() == MotionEvent.ACTION_MOVE) {
+//			Toast.makeText(ctxt, "Movement!", Toast.LENGTH_SHORT).show();
 		}
-			
-		int x = (int) (motion.getX() / squareSize);
-		int y = (int) (motion.getY() / squareSize);
+
+		int x = (int) (motion.getX() / SQUARE_SIZE);
+		int y = (int) (motion.getY() / SQUARE_SIZE);
 		currentX = x;
 		currentY = y;
 		this.invalidate();
---
+++ src/dadeindustries/game/gc/Ship.java
@@ -0,0 +1,13 @@
+package dadeindustries.game.gc;
+
+public class Ship {
+
+	int x = 0;
+	int y = 0;
+	
+	public Ship(int xinit, int yinit)
+	{
+		x = xinit;
+		y = yinit;
+	}
+}
