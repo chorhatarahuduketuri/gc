a0c840463ddbec9e72dd234798a5f0f83d0695ae
Adrian L. Shaw <adrianlshaw@gmail.com>
2018-06-16 23:14:56 +0100

    Added constructor to global game data

--- app/src/main/java/dadeindustries/game/gc/model/GlobalGameData.java
+++ app/src/main/java/dadeindustries/game/gc/model/GlobalGameData.java
@@ -1,15 +1,28 @@
 package dadeindustries.game.gc.model;
 
+import dadeindustries.game.gc.logic.Core;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * whereupon the overall definition of the universe is held. 
  * races, galaxy size, 
- * @author krongoth
+ * @author krongoth, adrianlshaw
  *
  */
 public class GlobalGameData {
 
-	public static enum Faction { UNITED_PLANETS, INIATS, GHZRGORZ, MORPHERS }
+	public enum Faction { UNITED_PLANETS, INIATS, GHZRGORZ, MORPHERS }
 	public static int galaxySizeX = 50;
 	public static int galaxySizeY = 50;
-	
+
+	public GlobalGameData(int x, int y) {
+		galaxySizeX = x;
+		galaxySizeY = y;
+	}
+
+	// Game data structures REFERENCE
+	protected ArrayList<Ship> ships;
+	protected Collection<Sector> sectors;
 }
