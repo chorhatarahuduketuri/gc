d3e2fb810ce6c62856abdaa9648b7a815039ec13
Adrian L. Shaw <adrianlshaw@gmail.com>
2018-06-16 23:18:01 +0100

    System now has a constructor and you can retrieve its coordinates

--- app/src/main/java/dadeindustries/game/gc/model/System.java
+++ app/src/main/java/dadeindustries/game/gc/model/System.java
@@ -3,13 +3,25 @@ package dadeindustries.game.gc.model;
 import java.util.ArrayList;
 
 
-public class SolarSystem {
+public class System {
 
 	private String name;
 	protected ArrayList<Planet> planets = null;
 
-	public SolarSystem(String name) {
+	private int x, y;
+
+	public System(String name, int x, int y) {
+		this.x = x;
+		this.y = y;
 		this.name = name;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
 	}
 
 	public String getName() {
