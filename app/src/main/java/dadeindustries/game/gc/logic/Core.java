01cd141c9871d1417378f60f3348c434c0bb5994
Adrian L. Shaw <adrianlshaw@gmail.com>
2018-06-16 23:14:10 +0100

    Core now loads some test systems by default

--- app/src/main/java/dadeindustries/game/gc/logic/Core.java
+++ app/src/main/java/dadeindustries/game/gc/logic/Core.java
@@ -2,10 +2,13 @@ package dadeindustries.game.gc.logic;
 
 import java.util.ArrayList;
 import java.util.Collection;
+
+import android.provider.Settings;
+import android.util.Log;
 import dadeindustries.game.gc.model.GlobalGameData;
 import dadeindustries.game.gc.model.Sector;
 import dadeindustries.game.gc.model.Ship;
-import dadeindustries.game.gc.model.SolarSystem;
+import dadeindustries.game.gc.model.System;
 
 /*
  * This is where all the data structures relevant 
@@ -19,8 +22,8 @@ public class Core {
 
 	//FIELDS
 	private Collection<Ship> ships = new ArrayList<Ship>();
-	private Collection<SolarSystem> solarSystems = new ArrayList<SolarSystem>();
-	private Sector[][] sectors = new Sector[50][50];
+	public Sector[][] sectors = new Sector[GlobalGameData.galaxySizeX][GlobalGameData.galaxySizeY];
+
 
 	//CONSTRUCTORS
 	public Core() {
@@ -30,6 +33,7 @@ public class Core {
 			}
 		}
 		loadTestShips();
+		loadTestPlanets();
 	}
 
 	//FUNCTIONS
@@ -43,7 +47,20 @@ public class Core {
 		ships.add(new Ship(sectors[3][3],
 				GlobalGameData.Faction.UNITED_PLANETS, "HMS Douglas"));
 		ships.add(new Ship(sectors[4][5],
-				GlobalGameData.Faction.MORPHERS, "Kdfkljsdf"));
+				GlobalGameData.Faction.MORPHERS, "ISS Yuri"));
+	}
+
+	private void loadTestPlanets() {
+		sectors[3][4] = new Sector(3,4,new System("Planet X",3,4));
+		sectors[4][4] = new Sector(4,4,new System("Planet Y",3,4));
+
+		for (int i = 0; i < GlobalGameData.galaxySizeX; i++) {
+			for (int j = 0; j < GlobalGameData.galaxySizeY; j++) {
+				if (sectors[i][j].hasSystem() == true) {
+					Log.wtf("Planet loaded", "Planet loaded at " + i + "," + j);
+				}
+			}
+		}
 	}
 
 	public void addShip(Ship newship) {
