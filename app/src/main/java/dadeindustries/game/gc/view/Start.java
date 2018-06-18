3f7a582442154f3d27143ecd6a561a700d770f0f
Adrian L. Shaw <adrianlshaw@gmail.com>
2018-06-18 20:42:10 +0100

    Music now pauses/resumes when app is suspended/reopened. Code is also reformatted.

--- app/src/main/java/dadeindustries/game/gc/view/Start.java
+++ app/src/main/java/dadeindustries/game/gc/view/Start.java
@@ -14,58 +14,72 @@ import android.media.MediaPlayer;
 
 public class Start extends Activity {
 
-	private GalaxyView gv;
-	private Core core;
+    private GalaxyView gv;
+    private Core core;
+    MediaPlayer mediaPlayer;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		core = new Core();
+        core = new Core();
         gv = new GalaxyView(this, core);
 
-		MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.lj_kruzer_chantiers_navals);
-		mediaPlayer.setLooping(true);
-		mediaPlayer.start(); // TODO: switch to async preparation method
+        mediaPlayer = MediaPlayer.create(this, R.raw.lj_kruzer_chantiers_navals);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start(); // TODO: switch to async preparation method
 
-		setContentView(gv);
-	}
+        setContentView(gv);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	/*
-	 * (non-Javadoc) Override the back button on Android phone to do what WE want
-	 * @see android.app.Activity#onBackPressed()
-	 */
-	@Override
-	public void onBackPressed() {
+    /*
+     * (non-Javadoc) Override the back button on Android phone to do what WE want
+     * @see android.app.Activity#onBackPressed()
+     */
+    @Override
+    public void onBackPressed() {
 
-		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
 
-		alertDialogBuilder.setTitle("GalaxyConquest");
+        alertDialogBuilder.setTitle("GalaxyConquest");
 
-		alertDialogBuilder
-				.setMessage("Do you wish to exit GC?")
-				.setCancelable(false)
-				.setPositiveButton("Yes",
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog, int id) {
-								finish();
-							}
-						})
-				.setNegativeButton("No", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						dialog.cancel();
-					}
-				});
+        alertDialogBuilder
+                .setMessage("Do you wish to exit GC?")
+                .setCancelable(false)
+                .setPositiveButton("Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
 
-		AlertDialog alertDialog = alertDialogBuilder.create();
-		alertDialog.show();
-	}
+                                finish();
+                            }
+                        })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onPause() {
+        mediaPlayer.pause();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
 }
