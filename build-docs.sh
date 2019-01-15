9cae09032f7e7ebc149e01f73987b07c198a79f4
Adrian L. Shaw <adrianlshaw@gmail.com>
2019-01-15 23:38:07 +0000

    Updated the build script to include classpath

--- build-docs.sh
+++ build-docs.sh
@@ -11,7 +11,12 @@ echo "Please wait while I find your android path in /home/$USER/"
 ANDROID_JAR=$(find ~ 2> /dev/null | grep "[0-9][0-9]/android.jar" | sort -u | tail -n 1)
 
 if [ -n "$ANDROID_JAR" ]; then
-	javadoc -protected -splitindex -d "$PWD/docs"  -bootclasspath "$ANDROID_JAR" -Xdoclint:none
+
+	FILES=$(find app/src | grep "\.java")
+	echo "$FILES" | awk -v P="$(pwd)" ' { printf "\"" P $1"" "\"\n" }  '
+
+	javadoc -protected -splitindex -d "$PWD/docs" -classpath "$CLASSPATH" \
+	       	-bootclasspath "$ANDROID_JAR" -Xdoclint:none
 else
 	echo "Couldn't find the Android path. Failed to build the docs :("	
 	exit 2
